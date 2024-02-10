8de7ecf2f38125383a4c6b6fc7ac4d3c08a37ba2
Vulpes231 <whykayshot@gmail.com>
2024-02-10 10:50:19 +0100

    fixed

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5200"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5200",
+  "https://sendmailportfolio.onrender.com",
+];
 
 module.exports = { allowedOrigins };
